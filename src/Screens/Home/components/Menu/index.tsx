339c83a3b57a7289f8d23b13c05cc4ffcc0fbd84
JoaoVitorAlbuquerque <joaovitorparussoloalbuquerque@gmail.com>
2024-09-06 20:06:13 -0300

    feat: create Menu in Home Screen

--- src/Screens/Home/components/Menu/index.tsx
+++ src/Screens/Home/components/Menu/index.tsx
@@ -1,3 +1,49 @@
+import { FlatList, TouchableOpacity } from "react-native";
+
+import { products } from '../../../../mocks/products';
+import { Text } from "../../../../components/Text";
+import { formatCurrency } from "../../../../utils/formatCurrency";
+
+import {
+  Product,
+  ProductImage,
+  ProductDetails,
+  Separator,
+  AddToCartButton,
+} from './styles';
+import { PlusCircle } from "../../../../components/Icons/PlusCircle";
+
 export function Menu() {
-  return null;
+  return (
+    <FlatList
+      data={products}
+      style={{ marginTop: 32 }}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
+      ItemSeparatorComponent={Separator}
+      keyExtractor={product => product.id}
+      renderItem={({ item: product }) => (
+        <Product>
+          <ProductImage
+            source={{
+              uri: `http://10.0.0.100:3000/uploads/${product.imagePath}`,
+            }}
+          />
+
+          <ProductDetails>
+            <Text weight="600">{product.name}</Text>
+            <Text size={14} color="#666" style={{ marginVertical: 8 }}>
+              {product.description}
+            </Text>
+            <Text size={14} weight="600">
+              {formatCurrency(product.price)}
+            </Text>
+          </ProductDetails>
+
+          <AddToCartButton style={{ position: 'absolute' }}>
+            <PlusCircle />
+          </AddToCartButton>
+        </Product>
+      )}
+    />
+  );
 }
